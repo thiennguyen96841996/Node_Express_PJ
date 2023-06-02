7f83ddbf4c9ad8b161c0c83a7d814bf7ddaab5ad
thiennguyen96841996 <thiennguyen96841996@gmail.com>
2023-06-02 17:08:17 +0900

    Todo app create

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * Create mysql connect
+ */
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'db',
+  user: 'root',
+  password: 'rootpw',
+  database: 'express'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  connection.query(
+      `select * from tasks;`,
+      (error, results) => {
+        console.log(error);
+        console.log(results);
+        res.render('index', {
+          title: 'ToDo App',
+          todos: results,
+        });
+      }
+  );
+});
+
+router.post('/', function(req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
+  const todo = req.body.add;
+  connection.query(
+      `insert into tasks (content) values ('${todo}');`,
+      (error, results) => {
+        console.log(error);
+        res.redirect('/');
+      }
+  );
 });
 
 module.exports = router;
