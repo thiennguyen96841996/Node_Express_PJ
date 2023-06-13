687805cac5c21ddb993526930284f52679b52c99
thiennguyen96841996 <thiennguyen96841996@gmail.com>
2023-06-13 16:02:31 +0900

    passportを利用した認証を実装

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cookieSession = require("cookie-session");
-const secret = "secretCuisine123";
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,16 +11,6 @@ var signupRouter = require('./routes/signup');
 var signinRouter = require('./routes/signin');
 var logoutRouter = require('./routes/logout');
 var app = express();
-
-app.use(
-    cookieSession({
-      name: "session",
-      keys: [secret],
-
-      // Cookie Options
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    })
-);
 
 // view engine setup
 app.engine('ejs', require('express-ejs-extend'));
@@ -33,6 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+
+// authorization
+require("./config/passport")(app);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,35 +5,41 @@ const knex = require('../db/knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    const userId = req.session.userid;
-    const isAuth = Boolean(userId);
-
-    knex("tasks")
-    .select("*")
-    .then(function (results) {
-        console.log(results);
+    const isAuth = req.isAuthenticated();
+    if (isAuth) {
+        const userId = req.user.id;
+        knex("tasks")
+            .select("*")
+            .where({user_id: userId})
+            .then(function (results) {
+                res.render('index', {
+                    title: 'ToDo App',
+                    todos: results,
+                    isAuth: isAuth,
+                });
+            })
+            .catch(function (err) {
+                console.error(err);
+                res.render('index', {
+                    title: 'ToDo App',
+                    isAuth: isAuth,
+                });
+            });
+    } else {
         res.render('index', {
             title: 'ToDo App',
-            todos: results,
             isAuth: isAuth,
         });
-    })
-    .catch(function (err) {
-        console.error(err);
-        res.render('index', {
-            title: 'ToDo App',
-            isAuth: isAuth,
-        });
-    });
+    }
 });
 
 router.post('/', function(req, res, next) {
-    const userId = req.session.userid;
-    const isAuth = Boolean(userId);
+    const isAuth = req.isAuthenticated();
+    const userId = req.user.id;
     const todo = req.body.add;
 
     knex("tasks")
-    .insert({content: todo})
+    .insert({user_id: userId, content: todo})
     .then(function () {
         res.redirect('/')
     })
